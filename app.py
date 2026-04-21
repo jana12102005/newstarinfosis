c265792e9e17debcb143a6c6e249e2528b5054c8
Janarthanan P <janarthanan12102005@gmail.com>
2026-04-22 00:10:07 +0530

    Fix string quotes in main block of app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os
 from app import create_app
 
+# Create Flask app instance for Gunicorn
 app = create_app()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(
-        debug=False,
-        host='0.0.0.0',
-        port=int(os.environ.get("PORT", 5000))
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
     )
